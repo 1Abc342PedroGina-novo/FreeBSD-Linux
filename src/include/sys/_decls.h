0a89e0187fd83b32001a2b7e8397175fbbc08706
1Abc342PedroGina-novo <rmagalhaesjuridico5@gmail.com>
2026-06-28 10:12:35 -0300

    Add header for C++ declaration compatibility

---
+++ src/include/sys/_decls.h
@@ -0,0 +1,12 @@
+#ifndef _SYS__DECLS_H_
+#define _SYS__DECLS_H_
+
+#if defined(__cplusplus)
+#define	__BEGIN_DECLS	extern "C" {
+#define	__END_DECLS	}
+#else
+#define	__BEGIN_DECLS
+#define	__END_DECLS
+#endif
+
+#endif
